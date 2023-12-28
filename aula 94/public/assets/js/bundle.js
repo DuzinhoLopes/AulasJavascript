0fa9136c3a78cf90cc598f442d58160fd3dcd3f2
Eduardo <eduardo.munhoz97@outlook.com>
2023-12-28 20:56:58 -0300

    ES6 Modules, import/export

---
+++ aula 94/public/assets/js/bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";console.log("Eduardo","Lopes",30),console.log(10)})();
+//# sourceMappingURL=bundle.js.map
---
+++ aula 94/src/modulo1.js
@@ -0,0 +1,9 @@
+const nome = 'Eduardo'
+const sobrenome = 'Lopes'
+const idade = 30
+
+function soma(x, y){
+  return x + y;
+}
+
+export { nome, sobrenome, idade, soma }
