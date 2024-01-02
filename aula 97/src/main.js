04c456356ff5fd7417adc175c93a4de3c26b7365
Eduardo <eduardo.munhoz97@outlook.com>
2024-01-02 18:03:25 -0300

    Exemplo de utilizacao do Js em navegadores antigos

---
+++ aula 97/src/main.js
@@ -0,0 +1,6 @@
+import 'core-js/stable'//soluções para utilização em navegardores mais antigos
+import 'regenerator-runtime/runtime'  //soluções para utilização em navegardores mais antigos
+
+import './assets/css/style.css'
+import executa from './module/promises'
+executa();
